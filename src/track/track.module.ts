6c90648f27307670f72e4b28def213db1363a890
Daniel Arango <d.arangoc2@uniandes.edu.co>
2023-11-24 09:20:06 -0500

    Arreglo para que run start funcione

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TrackEntity } from './track.entity';
+import { AlbumEntity } from 'src/album/album.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TrackEntity])],
+  imports: [TypeOrmModule.forFeature([TrackEntity, AlbumEntity])],
   providers: [TrackService]
 })
 export class TrackModule {}
